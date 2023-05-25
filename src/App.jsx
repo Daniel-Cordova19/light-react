07ef836282bb90b23bfc03e82d604a7772508662
Daniel Cordova <javierdanielcordova@gmail.com>
2023-05-25 14:20:29 +0200

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import LightOnImg from './assets/lighton.png'
 import LightOffImg from './assets/lightoff.png'
+import { useState } from 'react'
 
 function App() {
 
@@ -11,12 +12,33 @@ function App() {
   )
 }
 
+// 1. Vamos a transofrmar la parte estática en una parte dinámica usando el useState, PERO sin usar eventos
+
 function Light() {
+
+  // El estado de nuestra app, es que al principio, la bombilla está apagada
+  // off: apagada
+  // on: encendida
+  const [statusLight, setStatusLight] = useState("on")
+
+  function handleLightOn() {
+    setStatusLight("on");
+  }
+
+  function handleLightOff() {
+    setStatusLight("off");
+  }
 
   return <div>
     <h1>Click the buttons to turn on or off the light</h1>
-    <img src={LightOnImg} />
-    <div><button>Turn On</button><button>Turn Off</button></div>
+    <img src={statusLight === 'on' ? LightOnImg : LightOffImg} />
+    <div>
+      <button onClick={handleLightOn} style={{
+        display: statusLight == "on" ? 'none' : 'inline'
+      }}>Turn On</button>
+      <button onClick={handleLightOff} style={{
+        display: statusLight == "on" ? 'inline' : 'none'
+      }}>Turn Off</button></div>
   </div>
 }
 
